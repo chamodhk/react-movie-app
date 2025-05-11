c27757c188663c87635c2699ae374c0629efc873
Chamodh Nethsara <72559423+chamodhk@users.noreply.github.com>
2025-05-11 14:33:31 +0530

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,19 @@ import MovieCard from "./components/MovieCard";
 import Favorite from "./pages/Favorites";
 import Home from "./pages/Home";
 import { Routes, Route } from "react-router-dom";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
-    <main className="main-content">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/favorites" element={<Favorite />} />
-      </Routes>
-    </main>
+    <div>
+      <NavBar></NavBar>
+      <main className="main-content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/favorites" element={<Favorite />} />
+        </Routes>
+      </main>
+    </div>
   );
 }
 
